3d8a1321da1a8854cf22ac863dfc31404ec3d494
michael parker <mparker@thoughtworks.com>
2018-05-07 12:18:12 +1000

    challenge #1 solution: update onListen to detect positive y axis movement, print to console for feedback

--- src/Frame.js
+++ src/Frame.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Gyroscope, ScreenOrientation} from 'expo';
 
-const Y_AXIS_VAL = 6;
+export const Y_AXIS_VAL = 6;
 const GYRO_UPDATE_INTERVAL = 1000;
 
 class Frame extends Component {
@@ -10,7 +10,7 @@ class Frame extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gyroState: {x: 0, y: 0, z: 0},
+      gyroReading: 0,
     };
     this.onListen = this.onListen.bind(this);
     Gyroscope.setUpdateInterval(GYRO_UPDATE_INTERVAL);
@@ -21,23 +21,16 @@ class Frame extends Component {
   }
 
   onListen = (result) => {
-    this.setState({gyroState: result});
-  };
-
-  round = (n) => {
-    if (!n) {
-      return 0;
+    if (result.y >= Y_AXIS_VAL) {
+      this.setState({gyroReading: result.y});
+      console.log('tipped upwards');
     }
-    return Math.floor(n * 100) / 100;
   };
 
   render() {
-    const {x, y, z} = this.state.gyroState;
     return (
     <View>
-      <Text style={styles.textStyle}>x: {this.round(x)}</Text>
-      <Text style={styles.textStyle}>y: {this.round(y)}</Text>
-      <Text style={styles.textStyle}>z: {this.round(z)}</Text>
+      <Text style={styles.textStyle}>GuessIt</Text>
     </View>
     )
   }
--- src/Frame.spec.js
+++ src/Frame.spec.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Frame, {Y_AXIS_VAL} from './Frame';
 
 configure({adapter: new Adapter()});
 
 describe('Guess It App', () => {
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<Frame/>);
+    wrapper.setState({
+      gyroReading: 0
+    });
+  });
+
+  describe('Frame', () => {
+    test('should update gyroReading in state when face is tipped upwards', () => {
+      wrapper.instance().onListen({y: Y_AXIS_VAL + 1});
+      expect(wrapper.state('gyroReading')).toEqual(Y_AXIS_VAL + 1);
+    });
+
+    test('should not update gyroReading in state when face is tipped upwards, but with insufficient speed',  () => {
+      wrapper.instance().onListen({y: Y_AXIS_VAL - 1});
+      expect(wrapper.state('gyroReading')).toEqual(0);
+    });
+  })
 });
